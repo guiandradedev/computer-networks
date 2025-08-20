ab9ca9c695d5096b74415eebc9d58d55b3cef189
Ximenes <guiximenes902@gmail.com>
2025-08-20 18:35:46 -0300

    connection manager instanciado

--- client.py
+++ client.py
@@ -16,66 +16,56 @@ class Colors:
     UNDERLINE = '\033[4m'
 
 class Client:
-    def __init__(self, host='127.0.0.1', port=8000):
-        self.host = host
-        self.port = port
-        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._is_connected = False
-        
+    def __init__(self,host='127.0.0.1', port=8000):
+        self.connection = ConnectionManager(host,port)
+        self._stop_event = threading.Event()
+        self._receiver_thread = None
     def start(self):
-        try:
-            host = (self.host, self.port)
-            self._client.connect(host)
-            self._is_connected = True
-            print(f"Connected to {self.host}:{self.port}")
-        except Exception as e:
-            print(f"Failed to connect: {e}")
+        self.connection.establish_connection()
+        if not self.connection._is_connected:
             return
 
         self._stop_event = threading.Event()
         self._receiver_thread = threading.Thread(target=self.handle_response)
         self._receiver_thread.start()
-
         self.run_client()
 
     def send_message(self, message):
-        if not self._is_connected:
-            print("Not connected to server")
-            return False
-        
-        try:
-            self._client.send(message.encode("utf-8")[:1024])
-        except Exception as e:
-            print(f"Error sending message: {e}")
+        self.connection.send_data(message)
 
     def close_connection(self):
-        # Finaliza conexão em caso de erro ou disconnect
         self._stop_event.set()
-        self._is_connected = False
-
-        # Caso seja disconnect, finaliza o socket e a thread
-        if self._client:
-            try:
-                self._client.close()
-            except:
-                pass
-        
+        self.connection.close()
         if self._receiver_thread and self._receiver_thread.is_alive():
             self._receiver_thread.join(timeout=2)
+        print("Connection closed")
+        # # Finaliza conexão em caso de erro ou disconnect
+        # self._stop_event.set()
+        # self._is_connected = False
 
-        try:
-            sys.exit(130)
-        except SystemExit:
-            os._exit(130)
+        # # Caso seja disconnect, finaliza o socket e a thread
+        # if self._client:
+        #     try:
+        #         self._client.close()
+        #     except:
+        #         pass
+        
+        # if self._receiver_thread and self._receiver_thread.is_alive():
+        #     self._receiver_thread.join(timeout=2)
+
+        # try:
+        #     sys.exit(130)
+        # except SystemExit:
+        #     os._exit(130)
 
     def handle_response(self):
-        while not self._stop_event.is_set() and self._is_connected:
+        while not self._stop_event.is_set() and self.connection._is_connected:
             try:
-                response = self._client.recv(4096)
+                response = self.connection.receive_data()
                 if not response:
                     print("Server closed the connection.")
                     self.close_connection()
-                    
+                    break
                 response = response.decode("utf-8")
                 try:
                     response_json = json.loads(response)
@@ -104,7 +94,7 @@ class Client:
 
     def run_client(self):
         try:
-            while self._is_connected:
+            while self.connection._is_connected:
                 msg = input("Enter message: ")
                 
                 if msg == "":
@@ -119,10 +109,50 @@ class Client:
             print("interrupted by user")
         finally:
             self._stop_event.set()
-            self._client.close()
+            self.connection.close()
         
         self._receiver_thread.join()
         print("Connection to server closed")
+
+
+class ConnectionManager:
+    def __init__(self, host='127.0.0.1', port=8000):
+        self.host = host
+        self.port = port
+        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._is_connected = False
+    
+    def establish_connection(self):
+        try:
+            self._client.connect((self.host, self.port))
+            self._is_connected = True
+            print(f"Connected to {self.host}:{self.port}")
+        except Exception as e:
+            print(f"Failed to connect: {e}")
+            self._is_connected = False
+
+    def send_data(self,message:str):
+        if not self._is_connected:
+            print("Not connected to server")
+            return False
+        try:
+            self._client.send(message.encode("utf-8")[:1024])
+        except Exception as e:
+            print(f"Error sending message: {e}") 
+    
+    def receive_data(self):
+        try:
+            return self._client.recv(4096)
+        except Exception as e:
+            print(f"Error receiving data: {e}")
+            return None
+
+    def close(self):
+        self._is_connected = False
+        try:
+            self._client.close()
+        except:
+            pass
 
 if __name__ == '__main__': 
     client = Client()
